6bbce3890234fe8b332662b3ff6fd0072e33fd3f
John Schwartzman <john@fortesystem.com>
2026-01-05 16:06:02 -0500

    Refine comments in maketest.sh
    Updated comments for clarity and consistency in maketest.sh.

--- maketest.sh
+++ maketest.sh
@@ -3,11 +3,11 @@
 # maketest.sh
 # John Schwartzman, Forte Systems, Inc. 06/03/2019
 #
-# A makefile helper script to manage .debug and .release makefiles 
+# A makefile helper script to manage debug and release makefiles 
 # using the same source, object and executable files.
-# In Makefile use:  @source ../maketest.sh && test .release .debug
-#					@source ../maketest.sh && test .debug .release
-# Invoke Makefile with make .release, make .debug
+# In Makefile use:  @source ../maketest.sh && test release debug
+#					@source ../maketest.sh && test debug release
+# Invoke Makefile with make release or make debug
 #
 #############################################################################
 function test()
